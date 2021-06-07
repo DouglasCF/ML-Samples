238043c570f61525f2d52600c66beb079467000e
Douglas Fornaro <douglasfornaro@gmail.com>
2021-06-07 09:34:05 -0300

    Implement image labeling

--- app/src/main/java/br/com/fornaro/mlsamples/image_labeling/ImageLabelingFragment.kt
+++ app/src/main/java/br/com/fornaro/mlsamples/image_labeling/ImageLabelingFragment.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.FileProvider
@@ -11,6 +12,9 @@ import br.com.fornaro.mlsamples.BaseFragment
 import br.com.fornaro.mlsamples.R
 import br.com.fornaro.mlsamples.databinding.FragmentImageLabelingBinding
 import coil.load
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.label.ImageLabeling
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
 import java.io.File
 
 class ImageLabelingFragment :
@@ -30,6 +34,27 @@ class ImageLabelingFragment :
             registerForActivityResult(ActivityResultContracts.TakePicture()) { success ->
                 if (success) {
                     binding.imageView.load(uri)
+                    val image = InputImage.fromFilePath(requireContext(), uri)
+
+                    val options = ImageLabelerOptions.Builder()
+                        .setConfidenceThreshold(.7f)
+                        .build()
+                    val labeler = ImageLabeling.getClient(options)
+                    labeler.process(image)
+                        .addOnSuccessListener { labels ->
+                            binding.resultText.text = null
+                            for (label in labels) {
+                                val text = label.text
+                                val confidence = label.confidence
+                                val index = label.index
+
+                                binding.resultText.append("$text $confidence $index\n")
+                            }
+                        }
+                        .addOnFailureListener {
+                            Toast.makeText(requireContext(), "Error: $it", Toast.LENGTH_SHORT)
+                                .show()
+                        }
                 }
             }
     }
